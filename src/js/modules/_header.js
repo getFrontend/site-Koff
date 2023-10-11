dd7aa008d8463d64a6d85d5530053aac1bf36d43
Sergey UP <sergey10goals@gmail.com>
2023-10-11 21:03:04 +0300

    last update .gitignore and header.js

--- src/js/modules/_header.js
+++ src/js/modules/_header.js
@@ -82,15 +82,6 @@ export class Header {
             favouriteLink.append(favouriteText, svg);
         })
 
-        // favouriteLink.innerHTML = `
-        //     <span class="header__link-text">Избранное</span>
-        //     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-        //         <path
-        //             d="M8.41301 13.8733C8.18634 13.9533 7.81301 13.9533 7.58634 13.8733C5.65301 13.2133 1.33301 10.46 1.33301 5.79332C1.33301 3.73332 2.99301 2.06665 5.03967 2.06665C6.25301 2.06665 7.32634 2.65332 7.99967 3.55998C8.67301 2.65332 9.75301 2.06665 10.9597 2.06665C13.0063 2.06665 14.6663 3.73332 14.6663 5.79332C14.6663 10.46 10.3463 13.2133 8.41301 13.8733Z"
-        //             stroke="#1C1C1C" stroke-linecap="round" stroke-linejoin="round" />
-        //     </svg>
-        // `;
-
         const cartLink = document.createElement('a');
         cartLink.classList.add('header__link');
         cartLink.href = '/cart';
